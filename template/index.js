81a841e892f8c6121d8a27f30e093cc1f78e8281
杨俊宁 <youngjuning@aliyun.com>
2020-02-20 15:35:38 +0800

    集成react-navigation

--- template/index.js
+++ template/index.js
@@ -2,11 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native'
+import { AppRegistry } from 'react-native'
 import { configure } from 'mobx'
 import './global'
+import 'react-native-gesture-handler'
 import App from './App'
-import {name as appName} from './app.json'
+import { name as appName } from './app.json'
 
 // 设置 mobx 不允许在动作外部修改状态
 configure({ enforceActions: 'observed' })

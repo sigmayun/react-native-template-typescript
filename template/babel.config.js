3e99245b33ddcd55a278941af548a5f0ab7e1ddb
杨俊宁 <youngjuning@aliyun.com>
2020-02-20 15:44:16 +0800

    自动 remove console 语句

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset', 'mobx'],
+  env: {
+    production: {
+      plugins: [['transform-remove-console', { exclude: ['info'] }]],
+    },
+  },
 }

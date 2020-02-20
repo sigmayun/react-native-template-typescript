5e8856e5d19fd704fe873178ec85d74667faf58c
杨俊宁 <youngjuning@aliyun.com>
2020-02-20 15:05:15 +0800

    引入mobx

--- template/index.js
+++ template/index.js
@@ -2,8 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native'
+import { configure } from 'mobx'
+import App from './App'
+import {name as appName} from './app.json'
 
-AppRegistry.registerComponent(appName, () => App);
+// 设置 mobx 不允许在动作外部修改状态
+configure({ enforceActions: 'observed' })
+
+AppRegistry.registerComponent(appName, () => App)

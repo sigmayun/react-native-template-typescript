7d3b4f091c805207c6fe65957f228abf36ac36ad
杨俊宁 <youngjuning@aliyun.com>
2020-02-21 16:57:41 +0800

    add common module

--- template/src/common/index.ts
+++ template/src/common/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('hello veronica_utils')
-}
+import utils from './utils'
+import enums from './enums'
+import stores from './stores'
+
+export default { utils, enums, stores }

7df556bcc5f038f2b51a8cb29e3c02e382f7eaeb
杨俊宁 <youngjuning@aliyun.com>
2020-02-24 17:56:35 +0800

    配置 KeyboardManager 和 dayjs

--- template/index.js
+++ template/index.js
@@ -4,11 +4,16 @@
 
 import { AppRegistry, Text, TextInput } from 'react-native'
 import 'react-native-gesture-handler'
+import KeyboardManager from 'react-native-keyboard-manager'
+import dayjs from 'dayjs'
 import { configure } from 'mobx'
 import './global'
 import App from './App'
 import { name as appName } from './app.json'
+import 'dayjs/locale/zh-cn'
 
+// dayjs 本地化
+dayjs.locale('zh-cn')
 // 设置 mobx 不允许在动作外部修改状态
 configure({ enforceActions: 'observed' })
 // 设置字体大小不随系统字体大小变化而变化
@@ -16,5 +21,10 @@ Text.defaultProps = { ...Text.defaultProps, allowFontScaling: false }
 TextInput.defaultProps = { ...TextInput.defaultProps, allowFontScaling: false }
 // 屏蔽黄屏警告
 console.disableYellowBox = true
+// ios键盘解决方案
+if (global.ios) {
+  KeyboardManager.setEnableAutoToolbar(false)
+  KeyboardManager.setShouldResignOnTouchOutside(true)
+}
 
 AppRegistry.registerComponent(appName, () => App)

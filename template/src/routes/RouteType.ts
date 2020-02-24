74053fb5d22692547f84147e83c98c720fad99bd
zhangyu <grewer@grewer.cn>
2020-02-24 20:31:03 +0800

    refactor: 函数声明优化 去除无关

--- template/src/routes/RouteType.ts
+++ template/src/routes/RouteType.ts
@@ -2,10 +2,12 @@ import { StackNavigationProp } from '@react-navigation/stack'
 import { RootStackParamList } from 'src/routes/RootStackParamList'
 import { RouteProp } from '@react-navigation/native'
 
-type DetailsScreenNavigationProp<RouteName extends keyof RootStackParamList> = StackNavigationProp<RootStackParamList, RouteName>
-type DetailsScreenRouteProp<RouteName extends keyof RootStackParamList> = RouteProp<RootStackParamList, RouteName>
+type routeNameKeys = keyof RootStackParamList
 
-export type RouteType<RouteName extends keyof RootStackParamList> = {
-  navigation: DetailsScreenNavigationProp<RouteName>
-  route: DetailsScreenRouteProp<RouteName>
+type ScreenNavigationProp<RouteName extends routeNameKeys> = StackNavigationProp<RootStackParamList, RouteName>
+type ScreenRouteProp<RouteName extends routeNameKeys> = RouteProp<RootStackParamList, RouteName>
+
+export type RouteType<RouteName extends routeNameKeys> = {
+  navigation: ScreenNavigationProp<RouteName>
+  route: ScreenRouteProp<RouteName>
 }

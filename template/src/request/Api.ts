63a11182c47ef8f4536601aff48ce811c8210f37
杨俊宁 <youngjuning@aliyun.com>
2020-02-29 15:23:25 +0800

    简化清理空参数的方法

--- template/src/request/Api.ts
+++ template/src/request/Api.ts
@@ -3,16 +3,28 @@ import qs from 'qs'
 import cleanDeep from 'clean-deep'
 
 const handleRequestConfig = (config: AxiosRequestConfig) => {
-  config.headers = cleanDeep(config.headers)
-  config.params = cleanDeep(config.params)
-  config.data = cleanDeep(config.data)
-  if (config.method === 'get') {
-    config.paramsSerializer = params =>
+  // 在发送请求之前做些什么
+  const Config = cleanDeep(config, {
+    emptyArrays: false,
+    emptyObjects: false,
+  })
+  if (Config.method === 'get') {
+    Config.paramsSerializer = params =>
       qs.stringify(params, {
         arrayFormat: 'repeat',
       })
   }
-  return config
+  return Config
+}
+
+const handleResponseSuccess = response => {
+  // 对响应数据做点什么
+  return response.data
+}
+
+const handleResponseFail = error => {
+  // 对响应错误做点什么
+  return Promise.reject(error)
 }
 
 class Api {
@@ -23,24 +35,10 @@ class Api {
       timeout: 10000,
       ...baseConfig,
     })
-
-    // 添加请求拦截器
-    this.instance.interceptors.request.use(
-      config => handleRequestConfig(config),
-      error => Promise.reject(error)
-    )
-
-    // 添加响应拦截器
-    this.instance.interceptors.response.use(
-      response => {
-        // 对响应数据做点什么
-        return response.data
-      },
-      error => {
-        // 对响应错误做点什么
-        return Promise.reject(error)
-      }
-    )
+    // 请求拦截器
+    this.instance.interceptors.request.use(handleRequestConfig, error => Promise.reject(error))
+    // 响应拦截器
+    this.instance.interceptors.response.use(handleResponseSuccess, handleResponseFail)
   }
 }
 
